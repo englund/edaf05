04b8f07fbc853f158b43924d7d18ca67314bc7f8
Victor Englund <victor.englund@gmail.com>
2015-04-21 12:16:06 +0200

    Added find in front of the method name minimum_spanning_tree

--- lab3/kruskal.py
+++ lab3/kruskal.py
@@ -12,16 +12,16 @@ class Kruskal:
     def add_edge(self, u, v, w):
         self.edges.add((u, v, w))
 
-    def minimum_spanning_tree(self):
-        minimum_spanning_tree = set([])
+    def find_minimum_spanning_tree(self):
+        find_minimum_spanning_tree = set([])
         total_weight = 0
         edges_sorted_by_weight = sorted(self.edges, key=lambda u: u[2])
         for u, v, w in edges_sorted_by_weight:
             if (self.uf.find(u) != self.uf.find(v)):
                 self.uf.union(u, v)
-                minimum_spanning_tree.add((u, v, w))
+                find_minimum_spanning_tree.add((u, v, w))
                 total_weight += w
-        return minimum_spanning_tree, total_weight
+        return find_minimum_spanning_tree, total_weight
 
 class UnionFind:
     def __init__(self):
@@ -59,5 +59,5 @@ if __name__ == '__main__':
             kruskal.add_edge(u, v, w)
         else:
             kruskal.add_node(line.translate(None, '\n"'))
-    minimum_spanning_tree, total_weight = kruskal.minimum_spanning_tree()
+    minimum_spanning_tree, total_weight = kruskal.find_minimum_spanning_tree()
     print total_weight
